fce91f6ab0ba812716074bd1a83d8be59e8f94b2
Kevin Altman <kevin@thinkrelevance.com>
2012-02-03 10:21:04 -0500

    Updating the site to repo overwrote local changes on the live site. Needed to get the sidebar in sync with those changes

--- sidebar.php
+++ sidebar.php
@@ -12,16 +12,15 @@
 <nav class="sidebar">
   <div class="grid_2 nav_col">
     <ul class="navigation">
-      <li><a href="#1">About</a></li>
-      <li><a href="#2">Tutorials</a></li>
-      <li><a href="#3">Daily Reads</a></li>
-      <li><a href="#4">Contact</a></li>
+      <li><a href="http://relovely.com/about">About</a></li>
+      <li><a href="http://relovely.com/tutorials">Tutorials</a></li>
+      <li><a href="http://relovely.com/daily-reads">Daily Reads</a></li>
     </ul>
 
     <ul class="social_icons">
       <li class="pinterest"><a href="http://pinterest.com/lynngrogan">pinterest</a></li>
-      <li class="twitter"><a href="#">twitter</a></li>
-      <li class="flickr"><a href="#">flickr</a></li>
+      <li class="twitter"><a href="https://twitter.com/#!/lynngrogan">twitter</a></li>
+      <li class="flickr"><a href="http://www.flickr.com/">flickr</a></li>
     </ul>
     
     <?php if ( ! dynamic_sidebar( 'primary-widget-area' ) ) : 
